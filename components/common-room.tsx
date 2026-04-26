497e5aefdcbdf87ba6b158010bb33081121f497a
Guilherme Foligno <guilhermefoligno@gmail.com>
2026-04-26 04:58:39 -0300

    Fix ForestTower gameMode to prevent online room creation error

--- components/common-room.tsx
+++ components/common-room.tsx
@@ -2230,7 +2230,7 @@ export default function CommonRoom({ onStartDuel: _onStartDuel, onCreateRoom, on
             potion,
             spells: selectedSpells,
             avatar,
-            gameMode: "torneio-offline",
+            gameMode: "teste" as any,
             userId: currentUser.id,
             username: currentUser.username,
             elo: currentUser.elo,
